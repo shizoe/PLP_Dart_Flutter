e87aecc5f31661179ecbb0c2735d732d7503931e
Mabo Mbebeta <mabombebeta@my.uopeople.edu>
2024-03-05 14:36:24 +0200

    controlflow commandline application

---
+++ PLP_Dart_CLI/dart_datatypes/bin/dart_controlflow.dart
@@ -0,0 +1,42 @@
+import 'package:dart_utils/dart_controlflow.dart';
+
+void main() {
+  if (score >= 60) {
+    message = 'You passed';
+  } else {
+    message = 'You failed';
+  }
+  // if else
+  void ifElseExample() {
+    // Your code for the if-else example goes here
+  }
+
+  ifElseExample();
+
+  // switch case
+  void switchCaseExample() {
+    // Your code for the switch case example goes here
+  }
+
+  switchCaseExample();
+
+  // for loop
+  void forLoopExample() {
+    // Your code for the for loop example goes here
+  }
+
+  forLoopExample();
+
+  // while loop
+  void whileLoopExample() {
+    // Your code for the while loop example goes here
+  }
+
+  whileLoopExample();
+
+  // do while loop
+  void doWhileLoopExample() {
+    // Your code for the do-while loop example goes here
+  }
+  doWhileLoopExample();
+}
---
+++ PLP_Dart_CLI/dart_datatypes/lib/dart_controlflow.dart
@@ -0,0 +1,5 @@
+const score = 83;
+
+String message = '';
+
+const message1 = (score >= 60) ? 'You passed' : 'You failed';
